8535c2719f592cefbe0703179a7c0a34a3864b57
konrado1995 <huku4@wp.pl>
2021-03-08 21:33:00 +0100

    Pierwszy commit w github

--- Zad1.py
+++ Zad1.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from mrjob.job import MRJob
+
+# Aby uruchomić job'a należy użyć komendy:
+# $ python nazwa_skyptu.py nazwa_pliku_wejściowego.txt
+# $ python zad1.py data.txt data2.txt
+
+class MRWordCount(MRJob):
+
+    def mapper(self, _, line):
+        yield 'chars', len(line)
+        yield 'words', len(line.split())
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRWordCount.run()
---
+++ zad2.py
@@ -0,0 +1,12 @@
+from mrjob.job import MRJob
+
+class MRSimpleJob(MRJob):
+
+    def mapper(self, _, value):
+        yield 'line', 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRSimpleJob.run()
